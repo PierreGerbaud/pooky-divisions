ea686c020bcbffd22dad0c93f810e5e8fa3742fb
PierreGerbaud <114285296+PierreGerbaud@users.noreply.github.com>
2024-01-17 10:33:13 +0100

    Update pooky-divisions-script.js

--- pooky-divisions-script.js
+++ pooky-divisions-script.js
@@ -1,14 +1,18 @@
-function calculateDivisions(tier) {
+function calculateDivisions(tier, playersPerDivision) {
     return Math.pow(2, tier - 1);
 }
 
 function updateTiersAndDivisions() {
-    let playerCount = document.getElementById('playerCount').value;
+    let playerCount = parseInt(document.getElementById('playerCount').value);
+    let minPlayers = parseInt(document.getElementById('minPlayers').value);
+    let maxPlayers = parseInt(document.getElementById('maxPlayers').value);
+    let averagePlayers = (minPlayers + maxPlayers) / 2;
+
     let tier = 1;
     let totalDivisions = 0;
 
-    while (totalDivisions < playerCount / 16) {
-        totalDivisions += calculateDivisions(tier);
+    while (totalDivisions * averagePlayers < playerCount) {
+        totalDivisions += calculateDivisions(tier, averagePlayers);
         tier++;
     }
 
